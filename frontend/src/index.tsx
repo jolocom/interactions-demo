5a14f6688bfe53afa2ff10d744474506b38f477f
mnzaki <mnzaki@gmail.com>
2020-09-01 14:05:55 +0200

    fix(frontend): use REACT_APP_ env var prefix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App'
 import { JolocomWebServiceClient } from '@jolocom/web-service-client'
 
-const jwtCommand = process.env.JWT_COMMAND || 'cd rpc_agent;\nyarn start'
-const hostport = process.env.SERVICE_HOSTPORT || 'localhost:9000'
-const serviceAPI = new JolocomWebServiceClient(hostport, '/jolo', false)
+const jwtCommand = process.env.REACT_APP_JWT_COMMAND || 'cd rpc_agent;\nyarn start'
+const hostport = process.env.REACT_APP_SERVICE_HOSTPORT || 'localhost:9000'
+const tls = !!process.env.REACT_APP_SERVICE_TLS || false
+const serviceAPI = new JolocomWebServiceClient(hostport, '/jolo', tls)
+
 ReactDOM.render(<App serviceAPI={serviceAPI} jwtCommand={jwtCommand}/>, document.getElementById('root'))
