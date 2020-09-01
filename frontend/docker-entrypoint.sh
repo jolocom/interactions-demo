3c10e08dadede45cb637df43fc1737fccc05a83f
mnzaki <mnzaki@gmail.com>
2020-09-01 14:12:53 +0200

    feat(frontend): build and copy out if BUILD_OUTPUT specified

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-set -e
+set -ex
+
+yarn install
+
+if [ "${BUILD_OUTPUT#-}" == "${BUILD_OUTPUT}" ]; then
+  yarn build &&
+  rm -rf "${BUILD_OUTPUT}" &&
+  cp -r build "${BUILD_OUTPUT}"
+fi
 
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- yarn "$@"
 fi
 
-yarn install && exec "$@"
+exec "$@"
