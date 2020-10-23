bb044ca2df822f5a5cf50ada06b923099d11bba9
Cristian Lungu <cristian.lungu@hsrw.org>
2020-10-23 14:00:27 +0200

    feat(auth): added authentication interaction container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,14 @@ import React, { useState, useEffect } from 'react'
 import './reset.css'
 import './App.css'
 import {
-  InteractionContainer,
   CredOfferContainer,
   PeerResolutionContainer,
-  CredShareContainer
+  CredShareContainer,
 } from './ui/interaction'
 import { EstablishChannelContainer } from './ui/establishChannel'
-import { InteractionType } from './config'
+import { AuthenticationContainer } from './ui/authenticationContainer'
 import { JolocomWebServiceClient } from '@jolocom/web-service-client'
 const jolocomLogo = require('./images/JO_icon.svg')
-
-interface State {
-  loading: boolean
-  qrCode: {
-    source: string
-  }
-}
 
 interface AppProps {
   jwtCommand: string
@@ -31,37 +23,40 @@ const App: React.FunctionComponent<AppProps> = ({ serviceAPI, jwtCommand }) => {
     serviceAPI.sendRPC('getCredentialTypes').then((credTypes: string[]) => {
       setAvailableCredTypes(credTypes)
     })
-    serviceAPI.sendRPC('getRequestableCredentialTypes').then((credTypes: string[]) => {
-      setRequestableCredTypes(credTypes)
-    })
+    serviceAPI
+      .sendRPC('getRequestableCredentialTypes')
+      .then((credTypes: string[]) => {
+        setRequestableCredTypes(credTypes)
+      })
   }, [serviceAPI])
 
   return (
     <React.Fragment>
-      <header className="c-header" style={{textAlign: 'center'}}>
-        <h1><img src={jolocomLogo} alt="Jolocom Logo" />&nbsp;Jolocom</h1>
+      <header className="c-header" style={{ textAlign: 'center' }}>
+        <h1>
+          <img src={jolocomLogo} alt="Jolocom Logo" />
+          &nbsp;Jolocom
+        </h1>
         <h1>Interactions Demo</h1>
       </header>
       <main className="main">
         <article className="c-qrcode-container">
-          <PeerResolutionContainer
+          <PeerResolutionContainer serviceAPI={serviceAPI} />
+          <EstablishChannelContainer
             serviceAPI={serviceAPI}
+            jwtCommand={jwtCommand}
           />
-          <EstablishChannelContainer serviceAPI={serviceAPI} jwtCommand={jwtCommand} />
 
           <CredOfferContainer
             serviceAPI={serviceAPI}
-            credTypes={availableCredTypes} />
+            credTypes={availableCredTypes}
+          />
 
           <CredShareContainer
             serviceAPI={serviceAPI}
             credTypes={requestableCredTypes}
           />
-          {/*
-          <InteractionContainer interactionType={InteractionType.Receive} />
-          <InteractionContainer interactionType={InteractionType.Share} />
-          <InteractionContainer interactionType={InteractionType.Auth} />
-          */}
+          <AuthenticationContainer serviceAPI={serviceAPI} />
         </article>
       </main>
     </React.Fragment>
