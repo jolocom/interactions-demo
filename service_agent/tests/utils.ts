bad14003d7f7e553b3ed1437d4203fcb86a665e9
Mina Nagy Zaki <mnzaki@gmail.com>
2021-02-16 13:54:35 +0100

    test: setup test environment

---
+++ service_agent/tests/utils.ts
@@ -0,0 +1,60 @@
+import { createConnection, getConnection, ConnectionOptions } from 'typeorm'
+import { JolocomTypeormStorage } from '@jolocom/sdk-storage-typeorm'
+import { Agent, JolocomSDK, NaivePasswordStore } from '@jolocom/sdk'
+
+const defaultAgentName = 'j'
+const defaultDidMethodName = 'jun'
+
+export async function createAgent(
+  name = defaultAgentName,
+  didMethodName = defaultDidMethodName,
+  pass?: string,
+) {
+  const sdk = await getSdk(name)
+
+  const didMethod = sdk.didMethods.get(didMethodName)
+  const passwordStore = pass
+    ? { getPassword: async () => pass }
+    : new NaivePasswordStore()
+  const agent = new Agent({
+    sdk,
+    passwordStore,
+    didMethod,
+  })
+  return agent
+}
+
+export async function destroyAgent(name = defaultAgentName) {
+  const conn = getConnection(name)
+  return conn.close()
+}
+
+export const getConnectionConfig = (name: string) => ({
+  name,
+  type: 'sqlite',
+  database: ':memory:',
+  dropSchema: true,
+  entities: ['node_modules/@jolocom/sdk-storage-typeorm/js/src/entities/*.js'],
+  synchronize: true,
+  logging: false,
+})
+
+export const getSdk = async (name: string) => {
+  const connConfig = getConnectionConfig(name) as ConnectionOptions
+  const conn = await createConnection(connConfig)
+  return new JolocomSDK({
+    storage: new JolocomTypeormStorage(conn),
+  })
+}
+
+/**
+ * Allow alice to resolve bob
+ */
+export async function meetAgent(alice: Agent, bob: Agent, both = true) {
+  const bobId = bob.idw.did.split(':')[2]
+  const bobEL = await bob.storage.eventDB.read(bobId)
+
+  await alice.didMethod.registrar.encounter(bobEL)
+
+  if (both) await meetAgent(bob, alice, false)
+}
