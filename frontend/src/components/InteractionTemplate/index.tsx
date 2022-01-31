fd016dc47c75341ca898e1d3a6bbbb2bb7ed8e9f
SBub <svetaibuben@gmail.com>
2022-01-31 20:36:08 +0100

    feat(interaction-template): adding support for including redirectUrl

--- frontend/src/components/InteractionTemplate/index.tsx
+++ frontend/src/components/InteractionTemplate/index.tsx
@@ -4,16 +4,18 @@ import QRCode from 'qrcode'
 import styles from './InteractionTemplate.module.css'
 import { InteractionBtn } from '../InteractionBtn'
 import { InteractionQR } from '../InteractionQR'
+import { InteractionInput } from 'components/InteractionInput'
 
 interface IInteractionTemplateProps {
   startHandler: () => Promise<{ qr?: string; jwt?: string; err?: string }>
   startText: string
   children: React.ReactNode
 }
-async function getDeepLinkQr(jwt: string) {
-  return await QRCode.toDataURL(
-    `https://jolocom.app.link/interact?token=${jwt}`,
-  )
+async function getDeepLinkQr(jwt: string, redirecturl?: string) {
+  const deeplink = `https://jolocom.app.link/interact?token=${jwt}${
+    !!redirecturl ? `&redirectUrl=${encodeURIComponent(redirecturl)}` : ''
+  }`
+  return await QRCode.toDataURL(deeplink)
 }
 export const InteractionTemplate: React.FC<IInteractionTemplateProps> = ({
   startHandler,
@@ -24,6 +26,7 @@ export const InteractionTemplate: React.FC<IInteractionTemplateProps> = ({
   const [deeplinkQR, setDeeplinkQR] = useState<string | undefined>()
   const [jwt, setJwt] = useState<string>()
   const [err, setErr] = useState<string | undefined>()
+  const [redirecturl, setRedirecturl] = useState<string>('')
 
   const startBtnHandler = async () => {
     const resp = await startHandler()
@@ -31,12 +34,11 @@ export const InteractionTemplate: React.FC<IInteractionTemplateProps> = ({
     setJwt(resp.jwt)
     setErr(resp.err)
   }
-
   useEffect(() => {
     if (jwt) {
-      getDeepLinkQr(jwt).then(setDeeplinkQR)
+      getDeepLinkQr(jwt, redirecturl).then(setDeeplinkQR)
     }
-  }, [jwt])
+  }, [jwt, redirecturl])
 
   return (
     <div className={styles['container']}>
@@ -59,6 +61,15 @@ export const InteractionTemplate: React.FC<IInteractionTemplateProps> = ({
               <p className={styles['qr-header']}>
                 Contains deeplink with token
               </p>
+              <p>Redirect url</p>
+              <div className={styles['redirect-form']}>
+                <InteractionInput
+                  withoutLabel
+                  label="Redirect url"
+                  value={redirecturl}
+                  setValue={setRedirecturl}
+                />
+              </div>
               <img
                 src={deeplinkQR}
                 className={styles['qr-code']}
